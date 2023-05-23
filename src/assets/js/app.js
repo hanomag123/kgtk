0a2a21b67c0185b10e24058c893bf32564a912a4
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2023-05-23 08:32:39 +0300

    new

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -129,7 +129,7 @@ document.addEventListener("DOMContentLoaded", () => {
       prevScrollpos = 0;
       currentScrollPos = 0;
     };
-    const num = xl.matches ? 50 : 100;
+    const num = xl.matches ? 50 : 150;
     if (currentScrollPos > num) {
       header.classList.add('header--active');
     } else {
@@ -182,6 +182,178 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     return wrapper;
+  }
+
+  const inputs = document.querySelectorAll('input,textarea');
+  if (inputs.length) {
+    inputs.forEach(el => {
+      el.value ? el.classList.add('havetext') : el.classList.remove('havetext');
+      el.addEventListener('input', function () {
+        this.value ? this.classList.add('havetext') : this.classList.remove('havetext');
+      });
+      el.addEventListener('focus', function () {
+        this.parentElement.classList.remove('error');
+      })
+    });
+  }
+
+  const swiper1slides = document.querySelectorAll('.reviews-swiper .swiper-slide');
+  const swiper = new Swiper('.teachers-swiper', {
+    loop: swiper1slides.length > 2,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    speed: 500,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.teachers .next',
+      prevEl: '.teachers .prev',
+    },
+  })
+
+  const swiper2slides = document.querySelectorAll('.reviews-swiper .swiper-slide');
+  const swiper2 = new Swiper('.reviews-swiper', {
+    loop: swiper2slides.length > 2,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    speed: 500,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.reviews .next',
+      prevEl: '.reviews .prev',
+    },
+  })
+
+  const swiper3 = new Swiper('.partners-swiper', {
+    loop: false,
+    slidesPerView: 4,
+    spaceBetween: 40,
+    speed: 500,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.partners-wrapper .next',
+      prevEl: '.partners-wrapper .prev',
+    },
+  })
+
+  const mobileSwipers = document.querySelectorAll('.news-swiper');
+  if (mobileSwipers.length) {
+    mobileSwipers.forEach(el => {
+      let swiper4 = null;
+      if (xl.matches && swiper4 === null) {
+        swiper4 = new Swiper(el, {
+          loop: true,
+          slidesPerView: 'auto',
+          spaceBetween: 0,
+
+        });
+      }
+      xl.addEventListener('change', () => {
+        if (xl.matches) {
+          swiper4 = new Swiper(el, {
+            loop: true,
+            slidesPerView: 'auto',
+            spaceBetween: 0,
+          });
+        } else {
+          swiper4.destroy(true, true);
+        }
+      })
+    });
+  }
+
+
+  var x, i, j, l, ll, selElmnt, a, b, c;
+  /* Look for any elements with the class "custom-select": */
+  x = document.getElementsByClassName("custom-select");
+  l = x.length;
+  for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /* For each element, create a new DIV that will act as the selected item: */
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /* For each element, create a new DIV that will contain the option list: */
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+      /* For each option in the original select element,
+      create a new DIV that will act as an option item: */
+      c = document.createElement("DIV");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener("click", function(e) {
+          /* When an item is clicked, update the original select box,
+          and the selected item: */
+          var y, i, k, s, h, sl, yl, sel;
+          s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+          sl = s.length;
+          h = this.parentNode.previousSibling;
+          sel = this.parentNode.previousSibling.classList.add('select-selected--active')
+          for (i = 0; i < sl; i++) {
+            if (s.options[i].innerHTML == this.innerHTML) {
+              s.selectedIndex = i;
+              h.innerHTML = this.innerHTML;
+              y = this.parentNode.getElementsByClassName("same-as-selected");
+              yl = y.length;
+              for (k = 0; k < yl; k++) {
+                y[k].removeAttribute("class");
+              }
+              this.setAttribute("class", "same-as-selected");
+              break;
+            }
+          }
+          h.click();
+      });
+      b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function(e) {
+      /* When the select box is clicked, close any other select boxes,
+      and open/close the current select box: */
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+  }
+  
+  function closeAllSelect(elmnt) {
+    /* A function that will close all select boxes in the document,
+    except the current select box: */
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+      if (elmnt == y[i]) {
+        arrNo.push(i)
+      } else {
+        y[i].classList.remove("select-arrow-active");
+      }
+    }
+    for (i = 0; i < xl; i++) {
+      if (arrNo.indexOf(i)) {
+        x[i].classList.add("select-hide");
+      }
+    }
+  }
+  
+  /* If the user clicks anywhere outside the select box,
+  then close all select boxes: */
+  document.addEventListener("click", closeAllSelect);
+
+
+  const accordionButton = document.querySelectorAll('.accordion-button');
+
+  if (accordionButton.length) {
+    accordionButton.forEach(el => {
+      el.addEventListener('click', function () {
+        this.classList.toggle('--active');
+        this.parentElement.classList.toggle('--active');
+      })
+    })
   }
 });
 
